fb253a71a018f673bf4f78f90a5affa2ceecc1cf
Adam Shields <adamenote@gmail.com>
2020-02-06 15:42:13 -0500

    First Commit

---
+++ src/ecommerce/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import ContactForm
+
+def home_page(request):
+    context = {
+        "title": "Home Page",
+        "content": "Welcome to the Home Page",
+    }
+    return render(requests, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "About Us",
+        "content": "Welcome to the About Page",
+    }
+    return render(requests, "home_page.html", context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact",
+        "content": "Welcome to the Contact Page",
+        "form": contact_form,
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
+
+# def home_page(requests):
+#     return render(requests, "home_page.html")
