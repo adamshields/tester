b3c14ef9586cc4fc09d15e793f6804653b931a08
Adam Shields <adamenote@gmail.com>
2020-02-06 17:42:07 -0500

    Step 2 - Configured basic authentication and form validators like clean, clean_email etc...

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,29 +1,26 @@
+from django.contrib.auth import authenticate, login, get_user_model
+# from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
-        "title": "Home Page",
-        "content": "Welcome to the Home Page",
+        "title":"Hello World!",
+        "content":" Welcome to the homepage.",
+
     }
-    return render(requests, "home_page.html", context)
+    if request.user.is_authenticated:
+        context["premium_content"] = "YEAHHHHHH"
+    return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
         "title": "About Us",
         "content": "Welcome to the About Page",
     }
-    return render(requests, "home_page.html", context)
-
-def login_page(request):
-    return render(request, "auth/login.html", {})
-
-def register_page(request):
-    return render(request, "auth/register.html", {})
-
-
+    return render(request, "home_page.html", context)
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
@@ -41,5 +38,45 @@ def contact_page(request):
     #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
-# def home_page(requests):
-#     return render(requests, "home_page.html")
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    #print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username  = form.cleaned_data.get("username")
+        password  = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated())
+        if user is not None:
+            #print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            #context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username  = form.cleaned_data.get("username")
+        email  = form.cleaned_data.get("email")
+        password  = form.cleaned_data.get("password")
+        new_user  = User.objects.create_user(username, email, password)
+        print(new_user)
+
+    return render(request, "auth/register.html", context)
