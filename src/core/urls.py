41a50c310a88ae6805a369a3f9c06b098c2d7298
Adam Shields <adamenote@gmail.com>
2020-02-06 19:40:39 -0500

    List and detail views class based and function based

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -14,6 +15,10 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv', product_list_view),
+    re_path(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    re_path(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
 ]
 
 if settings.DEBUG:
