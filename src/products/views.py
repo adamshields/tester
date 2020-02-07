61e57f36b18c5b3de6378b5865627f435e8f095d
Adam Shields <adamenote@gmail.com>
2020-02-07 12:14:24 -0500

    updated productdetail CBV to use model manager with get_object

--- src/products/views.py
+++ src/products/views.py
@@ -30,7 +30,7 @@ def product_list_view(request):
     return render(request, "products/list.html", context) 
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all() - Removed because Im using get_object below which is tied to model manager
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -38,6 +38,14 @@ class ProductDetailView(DetailView):
         print(context)
         # context['abc'] = 123
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product Does Not Exist Class Base View")
+        return instance
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
