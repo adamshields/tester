7981f360694243071d5aa74e7d737e1b2f2faf7c
Adam Shields <adamenote@gmail.com>
2020-02-07 12:20:14 -0500

    Basically showed how Mode Managers can interact with any given object showed how to use ModelManager to override default methods

--- src/products/views.py
+++ src/products/views.py
@@ -13,14 +13,17 @@ from .models import Product
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all() removed because im using get_queryset below
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
-
+    
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 def product_list_view(request):
     queryset = Product.objects.all()
@@ -46,7 +49,11 @@ class ProductDetailView(DetailView):
         if instance is None:
             raise Http404("Product Does Not Exist Class Base View")
         return instance
-
+    
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     #instance = Product.objects.get(pk=pk) #id
