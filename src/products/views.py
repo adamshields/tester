153cf6df029e89b3101c9f9b41696edc1bbd0b38
Adam Shields <adamenote@gmail.com>
2020-02-06 20:10:20 -0500

    changed product detail fbv queryset

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Product
 # Create your views here.
@@ -41,7 +42,22 @@ class ProductDetailView(DetailView):
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     #instance = Product.objects.get(pk=pk) #id
-    instance = get_object_or_404(Product, pk=pk)
+    #instance = get_object_or_404(Product, pk=pk)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print("huh?")
+
+    qs  = Product.objects.filter(id=pk)
+    #print(qs)
+    if qs.exists() and qs.count() == 1: # len(qs)
+        instance = qs.first()
+    else:
+        raise Http404("Product doesn't exist")
+
     context = {
         'object': instance
     }
