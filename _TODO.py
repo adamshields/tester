6ea8dec2d167a6b067e69086000d3e0825de5a74
Adam Shields <adamenote@gmail.com>
2020-02-07 21:17:33 -0500

    Columns & Rows Commit

--- _TODO.py
+++ _TODO.py
@@ -1,2 +1,4 @@
 # Need to come back and visit forloop.counter and available methods 
 # https://www.codingforentrepreneurs.com/courses/ecommerce/templates/forloop-counter-cycle
+
+# Columns & Rows Commit
