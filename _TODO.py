1af89974979e4f4a8fee87de6c6b80a2466058ba
Adam Shields <adamenote@gmail.com>
2020-02-08 18:59:34 -0500

    creating the search form first commit

--- _TODO.py
+++ _TODO.py
@@ -5,3 +5,6 @@
 # 1-a Basic Search View
 # Display Query to User
 # # 6:33 in video - added get_context_data to view so query now == self.request.get('q') about to use it in the template
+
+# Creating the Search form
+# https://www.codingforentrepreneurs.com/courses/ecommerce/search-component/creating-search-form
