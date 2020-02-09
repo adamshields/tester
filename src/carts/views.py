51432769642e31f6e46a05bbcf0bc779b25a03c9
Adam Shields <adamenote@gmail.com>
2020-02-09 17:01:52 -0500

    just show session key

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
 def cart_home(request):
-    print(request.session) # session object is on the request object by default
-    print(dir(request.session)) # This allows me to inspect the different methods available on request.session
+    # print(request.session) # session object is on the request object by default
+    # print(dir(request.session)) # This allows me to inspect the different methods available on request.session
     # request.session.set_expiry(300) # Sets the expiration in seconds of session # 300 seconds == 5 minutes then expire this session
-    # request.session.session_key # shows Session Key
-    'session_key', 'set_expiry'
-    #
+    key = request.session.session_key # shows Session Key
+    print(key)
     return render(request, "carts/home.html")
