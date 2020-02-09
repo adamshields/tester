c0cdfe252ccaf3be7b93c36024171ee8589727f4
Adam Shields <adamenote@gmail.com>
2020-02-09 16:52:08 -0500

    This allows me to inspect the different methods available on request.session

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 def cart_home(request):
     print(request.session) # session object is on the request object by default
+    print(dir(request.session)) # This allows me to inspect the different methods available on request.session
     return render(request, "carts/home.html")
