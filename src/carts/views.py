d787bc5057108c3248c8c72078c1ab4966e79ad1
Adam Shields <adamenote@gmail.com>
2020-02-09 16:58:48 -0500

    # Sets the expiration in seconds of session and # shows Session Key

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 def cart_home(request):
     print(request.session) # session object is on the request object by default
     print(dir(request.session)) # This allows me to inspect the different methods available on request.session
+    # request.session.set_expiry(300) # Sets the expiration in seconds of session
+    # request.session.session_key # shows Session Key
+    'session_key', 'set_expiry'
+    #
     return render(request, "carts/home.html")
