71bb8a2994c112bcaeef701f1e77a423ca1a465f
Adam Shields <adamenote@gmail.com>
2020-02-09 16:59:43 -0500

    # 300 seconds == 5 minutes then expire this session

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def cart_home(request):
     print(request.session) # session object is on the request object by default
     print(dir(request.session)) # This allows me to inspect the different methods available on request.session
-    # request.session.set_expiry(300) # Sets the expiration in seconds of session
+    # request.session.set_expiry(300) # Sets the expiration in seconds of session # 300 seconds == 5 minutes then expire this session
     # request.session.session_key # shows Session Key
     'session_key', 'set_expiry'
     #
