c1b3f78dbc6de9e54ca3190b860203fb6c1560c8
Adam Shields <adamenote@gmail.com>
2020-02-09 13:17:35 -0500

    Added another lookup for price

--- src/products/models.py
+++ src/products/models.py
@@ -33,7 +33,11 @@ class ProductQuerySet(models.query.QuerySet):
         return self.filter(featured=True, active=True)
 
     def search(self, query):
-        lookups = Q(title__icontains=query) | Q(description__icontains=query)
+        lookups = (
+            Q(title__icontains=query) | 
+            Q(description__icontains=query) |
+            Q(price__icontains=query)
+        )
         return self.filter(lookups).distinct()
 
 class ProductManager(models.Manager):
