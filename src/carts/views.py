a47c1e89897e4f3cd28c65e384d6c991ddb9df80
Adam Shields <adamenote@gmail.com>
2020-02-09 16:49:30 -0500

    shows the session object itself

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def cart_home(request):
+    print(request.session) # session object is on the request object by default
     return render(request, "carts/home.html")
