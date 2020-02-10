527028075680620979febd581c825ae2e2a80b10
Adam Shields <adamenote@gmail.com>
2020-02-10 11:09:24 -0500

    added method for cart_create and also moved that method into the view for cart_home

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 
 from .models import Cart
 
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    return cart_obj
 
 def cart_home(request):
     request.session['cart_id'] = "12"
     cart_id = request.session.get("cart_id", None) 
     if cart_id is None and isinstance(cart_id, int): 
-        cart_obj = Cart.objects.create(user=None)
+        cart_create()
         request.session['cart_id'] = cart_obj.id 
         print('New Cart Created')
     else:
