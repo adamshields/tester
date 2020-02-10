2a3d48d7aae80d60f635718ff5b8df5ad995cbd9
Adam Shields <adamenote@gmail.com>
2020-02-09 19:52:28 -0500

    create cart_obj

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+
+from .models import Cart
+
+
 def cart_home(request):
     cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
     if cart_id is None: # and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
-        print('Create New Cart')
+        # print('Create New Cart')
+        cart_obj = Cart.objects.create(user=None)
         request.session['cart_id'] = 12 # This is where we actually set it 
     else:
         print('Cart ID Exists')
