7406a4347db1ff9dc7b513bd3e45bc03ba80498f
Adam Shields <adamenote@gmail.com>
2020-02-09 19:42:51 -0500

    enhance view to compare

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
-    if cart_id is None:
+    if cart_id is None and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
         print('Create New Cart')
         request.session['cart_id'] = 12 # This is where we actually set it 
     else:
