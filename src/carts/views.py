8d6f02ae5e47aa8440eb96adedf24b4480121337
Adam Shields <adamenote@gmail.com>
2020-02-09 19:53:57 -0500

    set cart_obj.id to session

--- src/carts/views.py
+++ src/carts/views.py
@@ -9,7 +9,7 @@ def cart_home(request):
     if cart_id is None: # and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
         # print('Create New Cart')
         cart_obj = Cart.objects.create(user=None)
-        request.session['cart_id'] = cart_obj # set cart_id to session
+        request.session['cart_id'] = cart_obj.id # set cart_obj.id to session
     else:
         print('Cart ID Exists')
     return render(request, "carts/home.html")
