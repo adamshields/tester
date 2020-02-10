2955d07e3da520a67a6ee0930675a95c2b538198
Adam Shields <adamenote@gmail.com>
2020-02-09 20:02:33 -0500

    Removed del cart id on new session

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,13 +5,12 @@ from .models import Cart
 
 
 def cart_home(request):
-    del request.session['cart_id']
     cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
     if cart_id is None: # and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
-        # print('Create New Cart')
         cart_obj = Cart.objects.create(user=None)
         request.session['cart_id'] = cart_obj.id # set cart_obj.id to session
         print(cart_obj.id) # shows actualdynamic cart id for session
+        print('New Cart Created')
     else:
         print('Cart ID Exists')
         print(cart_id)
