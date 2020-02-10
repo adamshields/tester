e84bb839b3266fed3a2856b2d3b8f41d56a75e2d
Adam Shields <adamenote@gmail.com>
2020-02-09 19:39:35 -0500

    modified cart view but need to enhance more

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
-    # print(request.session) # session object is on the request object by default
-    # print(dir(request.session)) # This allows me to inspect the different methods available on request.session
-    # request.session.set_expiry(300) # Sets the expiration in seconds of session # 300 seconds == 5 minutes then expire this session
-    # key = request.session.session_key # shows Session Key
-    # print(key) # shows key only when logged in when not logged in it shows None as session Key
-    # request.session['first_name'] = 'Justin'# used in example
-    request.session['cart_id'] = 12 # Session Setter
-    request.session['user'] = request.user.username # you can use it like this though this shows username for the session # but the object request.user OBJECT itself you can't do
+    if cart_id is None:
+        print('Create New Cart')
+        request.session['cart_id'] = 12 # This is where we actually set it 
+    else:
+        print('Cart ID Exists')
     return render(request, "carts/home.html")
