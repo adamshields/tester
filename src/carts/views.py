c1d763a1a0033b7e8a64dd3b3040250c40d20409
Adam Shields <adamenote@gmail.com>
2020-02-09 19:59:58 -0500

    delete the initial cart id

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,6 +5,7 @@ from .models import Cart
 
 
 def cart_home(request):
+    del request.session['cart_id']
     cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
     if cart_id is None: # and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
         # print('Create New Cart')
