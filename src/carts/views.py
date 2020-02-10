f5a90af5034b40e3053f2b2d7df78561f4036aa1
Adam Shields <adamenote@gmail.com>
2020-02-09 20:05:19 -0500

    removed comments to clean up view

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,11 +5,10 @@ from .models import Cart
 
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None) # this says get the current cart id or none
-    if cart_id is None: # and isinstance(cart_id, int): # isinstance allows us to compare with python, so you want to make sure the object is type of that class
+    cart_id = request.session.get("cart_id", None) 
+    if cart_id is None: # and isinstance(cart_id, int): 
         cart_obj = Cart.objects.create(user=None)
-        request.session['cart_id'] = cart_obj.id # set cart_obj.id to session
-        print(cart_obj.id) # shows actualdynamic cart id for session
+        request.session['cart_id'] = cart_obj.id 
         print('New Cart Created')
     else:
         print('Cart ID Exists')
