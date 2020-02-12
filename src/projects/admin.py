f81a456bf63fffe9daf6f9a6f8aa7db75e6d1f21
Adam Shields <adamenote@gmail.com>
2020-02-11 19:57:07 -0500

    Before Migrations

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import Environment, Team, Server, 
+from .models import Project
+
+# class EnvironmentAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'slug']
+#     class Meta:
+#         model = Environment
+
+# admin.site.register(Environment, EnvironmentAdmin)
+
+
+# class TeamAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'slug']
+#     class Meta:
+#         model = Team
+
+# admin.site.register(Team, TeamAdmin)
+
+
+# class ServerAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'slug']
+#     class Meta:
+#         model = Server
+
+# admin.site.register(Server, ServerAdmin)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    class Meta:
+        model = Project
+
+admin.site.register(Project, ProjectAdmin)
--- src/projects/models.py
+++ src/projects/models.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 
 # GET IN THE HABIT OF THINKING HEY THERE IS SOME ACTION THATS GONNA BE PERFORMED
 # What do we want to do with Team
@@ -21,9 +22,43 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
+def pre_save_team(sender, instance, *args, **kwargs):
+	slug = slugify(instance.name)
+	instance.slug = slug
+    
+pre_save.connect(pre_save_team, sender=Team)
+
+class ProjectQuerySet(models.query.QuerySet):
+
+    def active(self):
+        return self.filter(active=True)
+    
+        
+class ProjectManager(models.Manager):
+    
+    def get_queryset(self):
+        return ProjectQuerySet(self.model, using=self._db)
+
+    def status(self): # This is needed if you want to call something like Product.objects.featured()
+        return self.get_queryset().status()
+
 class Project(models.Model):
+    PROJECT_STATUS_CHOICES = (
+    ('canceled', 'Canceled'),
+    ('process', 'Process'),
+    ('consultation', 'Consultation'),
+    ('testing', 'Lab Testing'),
+    ('build', 'Build'),
+    ('production', 'Production'),
+    ('decommissioning', 'Decommissioning'),
+    ('decommissioned', 'Decommissioned'),
+    )
     name           = models.CharField(max_length=120, unique=True, verbose_name='Project Name')
     slug           = models.SlugField(blank=True, unique=True)
+    status         = models.CharField(max_length=120, default='created', choices=PROJECT_STATUS_CHOICES)
+    active         = models.BooleanField(default=True)
+
+    objects = ProjectManager()
 
     class Meta:
         verbose_name_plural = ('Projects')
@@ -33,34 +68,68 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
-class Environment(models.Model):
-    name           = models.CharField(max_length=300, verbose_name='Project Environment Name')
-    slug           = models.SlugField(unique=True, null=True)
-    project        = models.ForeignKey(Project, verbose_name="Project Name", related_name="project_environment", to_field="name", on_delete=models.CASCADE)
+def pre_save_project(sender, instance, *args, **kwargs):
+	slug = slugify(instance.name)
+	instance.slug = slug
 
-    class Meta:
-        verbose_name_plural = ('Project Environments')
-        ordering = ['id']
-        db_table = 'project_environments'
+pre_save.connect(pre_save_project, sender=Project)
+
+# class EnvironmentManager(models.Manager):
     
-    def __str__(self):
-        return self.name
+#     def new_or_get(self, request):
+#         cart_id = request.session.get("cart_id", None)
+#         qs = self.get_queryset().filter(id=cart_id)
+#         if qs.count() == 1:
+#             new_obj = False
+#             cart_obj = qs.first()
+#             if request.user.is_authenticated and cart_obj.user is None:
+#                 cart_obj.user = request.user
+#                 cart_obj.save()
+
+#         else:
+#             cart_obj = Cart.objects.new(user=request.user)
+#             new_obj = True
+#             request.session['cart_id'] = cart_obj.id
+#         return cart_obj, new_obj
+
+#     def new(self, user=None):
+#         user_obj = None
+#         if user is not None:
+#             if user.is_authenticated:
+#                 user_obj = user
+#         return self.model.objects.create(user=user_obj)
 
 
-class Server(models.Model):
-    name           = models.CharField(max_length=200, unique=True, verbose_name='Server Name')
-    slug           = models.SlugField(unique=True, null=True)
+# class Environment(models.Model):
+#     name           = models.CharField(max_length=300, verbose_name='Project Environment Name')
+#     slug           = models.SlugField(unique=True, null=True)
+#     project        = models.ForeignKey(Project, verbose_name="Project Name", related_name="project_environment", to_field="name", on_delete=models.CASCADE)
+
+#     objects = EnvironmentManager()
+
+#     class Meta:
+#         verbose_name_plural = ('Project Environments')
+#         ordering = ['id']
+#         db_table = 'project_environments'
     
-    # FOREIGN RELATIONSHIPS
-    teams          = models.ManyToManyField(Team, verbose_name='server_teams_join', blank=True)
-    project        = models.ForeignKey(Project, verbose_name='server_project_join', on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.name
+
+
+# class Server(models.Model):
+#     name           = models.CharField(max_length=200, unique=True, verbose_name='Server Name')
+#     slug           = models.SlugField(unique=True, null=True)
+    
+#     # FOREIGN RELATIONSHIPS
+#     teams          = models.ManyToManyField(Team, verbose_name='server_teams', blank=True)
+#     project        = models.ForeignKey(Project, verbose_name='server_project', on_delete=models.CASCADE)
 
     
-    class Meta:
-        verbose_name_plural = ('Servers')
-        ordering = ['id', 'name']
-        db_table = 'servers'
+#     class Meta:
+#         verbose_name_plural = ('Servers')
+#         ordering = ['id', 'name']
+#         db_table = 'servers'
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
